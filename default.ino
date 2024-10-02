31dc1f8b7964fe6c3b69e6913ee44bc293027cca
DestiShell <154367510+DestiShell@users.noreply.github.com>
2024-10-02 21:59:20 +0200

    No update

---
+++ default.ino
@@ -0,0 +1,189 @@
+// Brain F@ck moment
+
+// Code make by c++ (using for Arduino / ESP 32 And another)
+
+const int NullIq = 12;
+int innt[NullIq] = {1,2,3,4,52,26,77,8,9,10,11,12};
+
+// я тебя помножу на 0 <-----
+#define Film Serial
+
+#define film quardo
+
+/*#define ?Karburator Dvigatel*?(*/#define Karburator Dvigatel
+
+
+
+
+#define define define52
+
+
+#define capybaraSpit capybaraSmotrit
+
+
+int Shit = 12000;
+
+
+
+
+
+/*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*/int LowIq = 1;
+
+int MyIq = 0;
+
+/*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*/int PapaNotOk = MyIq;
+
+/*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*/int superIq = 9600;
+
+/*код тут лол (too lazy)*/const int baud = 9600;
+
+/*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*/const int customdelay1 = 1200;
+
+int Karbon = 0;
+
+/*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*//*u898 * mY CUSTOM MEGA SUPER BEBRA INT YEYEYEEYEYEYEYEY INT INT INTI INT * *8*&*/int UrIq = Karbon;
+
+int PapaOk = MyIq;
+
+void Karburator(/*Тут могла быть ваша реклама*/) {
+
+for(int i=0; i<NullIq; i+=LowIq){
+
+innt[i]=random(1,1000); //может мне выпадет нормальный показатель IQ?
+
+
+}
+
+}
+
+void albert() {/*Brain time*/
+
+// тут типо код дааа :)
+
+// О нет уроки спорта :(
+
+for(int ok=PapaOk; ok<NullIq; ok+=LowIq){
+
+//Делаем цыганские фокусы
+
+for(int NotOk=ok; NotOk<NullIq; NotOk+=LowIq){
+
+if(innt[ok]>innt[NotOk]){
+
+UrIq = innt[ok]; 
+
+//framework
+
+innt[ok] = innt[NotOk];
+
+innt[NotOk] = UrIq;
+
+
+// /*/*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   */for(int/*   тут должен быть пробел... Хотя в чем смысл жызни   */NotOk=ok;/*   тут должен быть пробел... Хотя в чем смысл жызни   */NotOk<NullIq;/*   тут должен быть пробел... Хотя в чем смысл жызни   */NotOk+=LowIq){
+
+// /*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   */if(innt[ok]/*   тут должен быть пробел... Хотя в чем смысл жызни   */</*   тут должен быть пробел... Хотя в чем смысл жызни   */innt[NotOk]){/*   тут должен быть пробел... Хотя в чем смысл жызни   */
+
+// /*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   */UrIq/*   тут должен быть пробел... Хотя в чем смысл жызни   */=/*   тут должен быть пробел... Хотя в чем смысл жызни   */innt[ok];
+
+// /*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *////*   тут должен быть пробел... Хотя в чем смысл жызни   */framework
+
+// /*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   */innt[ok]/*   тут должен быть пробел... Хотя в чем смысл жызни   */=/*   тут должен быть пробел... Хотя в чем смысл жызни   */innt[NotOk];
+
+// /*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   */innt[NotOk]/*   тут должен быть пробел... Хотя в чем смысл жызни   */=/*   тут должен быть пробел... Хотя в чем смысл жызни   */UrIq;
+
+// /*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   */}
+
+// /*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   *//*   тут должен быть пробел... Хотя в чем смысл жызни   */}*/
+}
+
+}
+
+//Цыганские фокусы еее
+
+}
+
+}
+
+void reklama(/*тут могла быть не ваша реклама*/) {
+
+// тут типо код дааа :)
+
+// О нет уроки спорта :(
+
+for(int ok=PapaOk; ok<NullIq; ok+=LowIq){
+//  for(int ok=notOkeyBro; ok<Null; ok+=MaxIq){
+
+    // Делаем цыганские фокусы
+
+    for(int NotOk=ok; NotOk<NullIq; NotOk+=LowIq){
+
+        if(innt[ok] < innt[NotOk]){ 
+
+            UrIq = innt[ok];
+
+            // framework
+
+            innt[ok] = innt[NotOk]; // будто от перестановки что либо меняется
+
+            innt[NotOk] = UrIq; 
+
+        }
+
+    }
+
+    // Цыганские фокусы еее
+
+}
+
+}
+
+void capybaraSpit() {
+
+//code here plz
+
+// HELP ❌ | НЕГР ✔
+
+for(int ii = 0; ii < NullIq; /*Brain time*/ ii++) {
+
+Film.print(String(" ") + String( innt[ii]));
+
+}
+
+Film.println();
+
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  // to lazy
+  // to... (nothing)
+
+Serial.begin(baud);
+}
+
+void loop() {
+  //  Пекарня кряхтит :(
+    //CA-BOOM
+
+capybaraSmotrit();
+ // 俿
+/* innt[4] = 48;*/
+
+Dvigatel();
+Film.print(String("Д") /*Д*/ + "в" /*в*/ + "и" /*и*/ + "г" /*г*/ + "а" /*а*/ + "т" /*т*/ + "е" /*е*/ + "л" /*л*/ + "ь" /*ь*/ + " " /* */ + "з" /*з*/ + "а" /*а*/ + "п" /*п*/ + "у" /*у*/ + "щ" /*щ*/ + "е" /*е*/ + "н" /*н*/ + "," /*,*/ + " " /* */ + "н" /*н*/ + "а" /*а*/ + "ч" /*ч*/ + "и" /*и*/ + "н" /*н*/ + "а" /*а*/ + "е" /*е*/ + "м" /*м*/ + " " /* */ + "с" /*с*/ + "о" /*о*/ + "р" /*р*/ + "т" /*т*/ + "и" /*и*/ + "р" /*р*/ + "о" /*о*/ + "в" /*в*/ + "к" /*к*/ + "у" /*у*/);
+capybaraSmotrit(/*Что?*/);
+albert();
+Film.print(String("Д") /*Д*/ + "в" /*в*/ + "и" /*и*/ + "г" /*г*/ + "а" /*а*/ + "т" /*т*/ + "е" /*е*/ + "л" /*л*/ + "ь" /*ь*/ + " " /* */ + "з" /*з*/ + "а" /*а*/ + "г" /*г*/ + "л" /*л*/ + "о" /*о*/ + "х" /*х*/ + "," /*,*/ + " " /* */ + "с" /*с*/ + "о" /*о*/ + "р" /*р*/ + "т" /*т*/ + "и" /*и*/ + "р" /*р*/ + "о" /*о*/ + "в" /*в*/ + "к" /*к*/ + "а" /*а*/ + " " /* */ + "о" /*о*/ + "к" /*к*/ + "о" /*о*/ + "н" /*н*/ + "ч" /*ч*/ + "е" /*е*/ + "н" /*н*/ + "а" /*а*/);
+reklama();
+capybaraSmotrit(/*Что?*/);
+Film.print(String("Д") /*Д*/ + "в" /*в*/ + "и" /*и*/ + "г" /*г*/ + "а" /*а*/ + "т" /*т*/ + "е" /*е*/ + "л" /*л*/ + "ь" /*ь*/ + " " /* */ + "с" /*с*/ + "о" /*о*/ + "ш" /*ш*/ + "е" /*е*/ + "л" /*л*/ + " " /* */ + "с" /*с*/ + " " /* */ + "у" /*у*/ + "м" /*м*/ + "а" /*а*/ + "," /*,*/ + " " /* */ + "п" /*п*/ + "р" /*р*/ + "о" /*о*/ + "с" /*с*/ + "и" /*и*/ + "х" /*х*/ + "о" /*о*/ + "д" /*д*/ + "и" /*и*/ + "т" /*т*/ + " " /* */ + "о" /*о*/ + "т" /*т*/ + "к" /*к*/ + "а" /*а*/);
+capybaraSmotrit();
+delay(customdelay1);
+
+
+}
+
+// <. <>,.._--==- <>,....><,====== ><><>.>.,>,.> +==-=-,.,.<.. <hello world>
+
+// Кароч мне лень ууу и вааабще мине 4 года уууу у уу  ууу у у у 
+// pisya
